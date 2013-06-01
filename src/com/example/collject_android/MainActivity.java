b3f46f6d97124d12b1c04485da7afdb25bcce39f
robzenn92 <rob.zenn92@hotmail.it>
2013-06-01 15:03:22 +0200

    update

--- src/com/example/collject_android/MainActivity.java
+++ src/com/example/collject_android/MainActivity.java
@@ -1,15 +1,60 @@
 package com.example.collject_android;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
+
+public class MainActivity extends FragmentActivity implements TabListener {
+
+	private ActionBar actionBar;
+
+	private final Handler handler = new Handler();
+
+	private PagerSlidingTabStrip tabs;
+	private ViewPager pager;
+	private MyPagerAdapter adapter;
+
+	private Drawable oldBackground = null;
+	private int currentColor = 0xFF666666;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+		pager = (ViewPager) findViewById(R.id.pager);
+		adapter = new MyPagerAdapter(getSupportFragmentManager());
+
+		pager.setAdapter(adapter);
+
+		final int pageMargin = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+						.getDisplayMetrics());
+		pager.setPageMargin(pageMargin);
+
+		tabs.setViewPager(pager);
+
+		changeColor(currentColor);
 	}
 
 	@Override
@@ -17,6 +62,163 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void createTab(int view, int titleView, String title) {
+		ActionBar.Tab tab = getActionBar().newTab();
+		tab.setTabListener(this);
+		tab.setCustomView(view);
+		getActionBar().addTab(tab);
+		((TextView) findViewById(titleView)).setText(title);
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+
+		case R.id.action_settings:
+			QuickContactFragment dialog = new QuickContactFragment();
+			dialog.show(getFragmentManager(), "QuickContactFragment");
+			return true;
+
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void changeColor(int newColor) {
+
+		tabs.setIndicatorColor(newColor);
+
+		// change ActionBar color just if an ActionBar is available
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+			Drawable colorDrawable = new ColorDrawable(newColor);
+			Drawable bottomDrawable = getResources().getDrawable(
+					R.drawable.plus);
+			LayerDrawable ld = new LayerDrawable(new Drawable[] {
+					colorDrawable, bottomDrawable });
+
+			if (oldBackground == null) {
+
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					ld.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(ld);
+				}
+
+			} else {
+
+				TransitionDrawable td = new TransitionDrawable(new Drawable[] {
+						oldBackground, ld });
+
+				// workaround for broken ActionBarContainer drawable handling on
+				// pre-API 17 builds
+				// https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					td.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(td);
+				}
+
+				td.startTransition(200);
+
+			}
+
+			oldBackground = ld;
+
+			// http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+			getActionBar().setDisplayShowTitleEnabled(false);
+			getActionBar().setDisplayShowTitleEnabled(true);
+
+		}
+
+		currentColor = newColor;
+
+	}
+
+	public void onColorClicked(View v) {
+
+		int color = Color.parseColor(v.getTag().toString());
+		changeColor(color);
+
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("currentColor", currentColor);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		currentColor = savedInstanceState.getInt("currentColor");
+		changeColor(currentColor);
+	}
+
+	private Drawable.Callback drawableCallback = new Drawable.Callback() {
+		@Override
+		public void invalidateDrawable(Drawable who) {
+			getActionBar().setBackgroundDrawable(who);
+		}
+
+		@Override
+		public void scheduleDrawable(Drawable who, Runnable what, long when) {
+			handler.postAtTime(what, when);
+		}
+
+		@Override
+		public void unscheduleDrawable(Drawable who, Runnable what) {
+			handler.removeCallbacks(what);
+		}
+	};
+
+	public class MyPagerAdapter extends FragmentPagerAdapter {
+
+		private final String[] TITLES = { "Projects", "Problems", "Solutions",
+				"Top Free", "Top Grossing", "Top New Paid", "Top New Free",
+				"Trending" };
+
+		public MyPagerAdapter(android.support.v4.app.FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return TITLES[position];
+		}
+
+		@Override
+		public int getCount() {
+			return TITLES.length;
+		}
+
+		@Override
+		public android.support.v4.app.Fragment getItem(int position) {
+			return CardFragment.newInstance(position);
+		}
+
 	}
 
 }
