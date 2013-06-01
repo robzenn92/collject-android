cc5fc80213d090916d29b425276f3ad8547043aa
Giulio Fornasaro <forna.991@gmail.com>
2013-06-01 17:29:11 +0200

    added the class to show the detailed data of the problems

---
+++ src/com/example/collject_android/ProblemEnlarged.java
@@ -0,0 +1,19 @@
+package com.example.collject_android;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ProblemEnlarged extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		this.setContentView(R.layout.activity_problem_enlarged);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+}
